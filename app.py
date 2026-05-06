a7ad6ca3ecef9d626dc9dc3c53c72f4bb903113c
Krunal <nsundersia@gmail.com>
2026-05-06 15:11:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, redirect, url_for, session, jsonify
 from authlib.integrations.flask_client import OAuth
 from dotenv import load_dotenv
 
-# Load environment vari
+# Load environment var
 load_dotenv()
 
 # Configure logging
